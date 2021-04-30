a7817604b9e15a80ede9632736c305f242dc1d35
cperez <chrper31@gmail.com>
2021-04-30 12:32:02 +0200

    Initial commit

---
+++ src/homeshop/Customer.java
@@ -0,0 +1,15 @@
+package homeshop;
+
+public class Customer {
+    private String fullname;
+    private String address;
+
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
